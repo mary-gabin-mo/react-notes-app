da11ff1094a16fd29e2e0a9e72c3642033d72002
Mary Mo <mo.mary.gabin@gmail.com>
2024-06-18 13:10:09 -0600

    Adding Notes feature added

--- src/components/NotesList.js
+++ src/components/NotesList.js
@@ -1,13 +1,15 @@
 import Note from './Note';
 import AddNote from './AddNote';
 
-const NotesList = ({ notes }) => {
+const NotesList = ({ notes, handleAddNote }) => {
     return (
         <div className='notes-list'>
             {notes.map((note) => (
                 <Note id={note.id} text={note.text} date={note.date}/>
             ))}
-            <AddNote />
+            <AddNote handleAddNote={handleAddNote} /> 
+            {/* above = prop drilling: passing the method through to the next level
+             */}
         </div>
     );
 };
