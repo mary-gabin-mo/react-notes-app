1eebb6bc8ec340853c2c96657df5626898cc2dbb
Mary Mo <mo.mary.gabin@gmail.com>
2024-06-18 10:14:51 -0600

    progress 2 - Feature: adding new notes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { nanoid } from 'nanoid';
+import { nanoid } from "nanoid";
 import NotesList from "./components/NotesList";
 
 const App = () => {
@@ -7,11 +7,24 @@ const App = () => {
     id: nanoid(),
     text: "This is my first note!",
     date: "17/06/2024"
-  }]);
+  },{
+    id: nanoid(),
+    text: "This is my second note!",
+    date: "17/06/2024"
+  },{
+    id: nanoid(),
+    text: "This is my third note!",
+    date: "19/06/2024"
+  },{
+    id: nanoid(),
+    text: "This is my new note!",
+    date: "20/06/2024"
+  },
+]);
 
   return (
     <div className='container'>
-      <NotesList />
+      <NotesList notes={notes}/>
     </div>
   )
 };
