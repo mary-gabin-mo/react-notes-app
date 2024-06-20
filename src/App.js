2123d2b5658ae9c60949906722e61d22654cfcd7
marymo <maostar1010@gmail.com>
2024-06-20 02:19:20 -0600

    save feature added... but not working?

--- src/App.js
+++ src/App.js
@@ -1,48 +1,76 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { nanoid } from "nanoid";
 import NotesList from "./components/NotesList";
 import Search from "./components/Search";
 import Header from "./components/Header";
 
 const App = () => {
-  const [notes, setNotes] = useState([{
-    id: nanoid(),
-    text: "This is my first note!",
-    date: "17/06/2024"
-  },{
-    id: nanoid(),
-    text: "This is my second note!",
-    date: "17/06/2024"
-  },{
-    id: nanoid(),
-    text: "This is my third note!",
-    date: "19/06/2024"
-  },{
-    id: nanoid(),
-    text: "This is my new note!",
-    date: "20/06/2024"  
-  },
-]);
+  const [notes, setNotes] = useState([
+    {
+      id: nanoid(),
+      text: "This is my first note!",
+      date: "17/06/2024"
+    },{
+      id: nanoid(),
+      text: "This is my second note!",
+      date: "17/06/2024"
+    },{
+      id: nanoid(),
+      text: "This is my third note!",
+      date: "19/06/2024"
+    },{
+      id: nanoid(),
+      text: "This is my new note!",
+      date: "20/06/2024"  
+    },  
+  ]);
 
-const [searchText, setSearchText] = useState('');
+  const [searchText, setSearchText] = useState('');
 
-const [darkMode, setDarkMode] = useState(false);
+  const [darkMode, setDarkMode] = useState(false);
 
-const addNote = (text) => {
-  const date = new Date();
-  const newNote = {
-    id: nanoid(),
-    text: text,
-    date: date.toLocaleDateString()
+  // Loading the notes from the local storage when the app loads
+  useEffect(() => {
+    const savedNotes = JSON.parse(
+      localStorage.getItem('react-notes-app-data')
+    );
+
+    // check if any notes are retrieved
+    if (savedNotes !== null){
+      setNotes(savedNotes);
+    }
+
+  }, []);
+  // when the dependency array is empty in the effect hook, 
+  //it's only going to run on the first load and won't run after that
+
+  // Saving the notes to a local storage
+  useEffect(() => {
+    localStorage.setItem(
+      'react-notes-app-data', // key
+      JSON.stringify(notes)
+    );
+  }, [notes]);
+  // this is a good approach because any time the notes array changes, 
+  // this will trigger automatically
+  // eg. if adding an edit feature, as long as you are saving the new notes, 
+  // the state change will trigger this
+
+  const addNote = (text) => {
+    const date = new Date();
+    const newNote = {
+      id: nanoid(),
+      text: text,
+      date: date.toLocaleDateString()
+    };
+    const newNotes = [...notes, newNote];
+    setNotes(newNotes);
   };
-  const newNotes = [...notes, newNote];
-  setNotes(newNotes);
-};
 
-const deleteNote = (id) => {
-  const newNotes = notes.filter((note)=> note.id !== id);
-  setNotes(newNotes);
-}
+  const deleteNote = (id) => {
+    const newNotes = notes.filter((note)=> note.id !== id);
+    setNotes(newNotes);
+  };
 
   return (
     <div className={`${darkMode && 'dark-mode'}`}>
@@ -59,7 +87,7 @@ const deleteNote = (id) => {
         />
       </div>
     </div>
-  )
+  );
 };
 
 export default App;
